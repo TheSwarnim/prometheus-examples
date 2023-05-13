982ff7820ccb5328ca94336d7b6cb083b7d537a7
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2023-05-14 03:22:04 +0530

    added springboot example

---
+++ springboot_example/src/main/java/com/springboot_example/springboot_example/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.springboot_example.springboot_example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+   
+    @GetMapping("/hello")
+    public String hello() {
+        return "World";
+    }
+}
