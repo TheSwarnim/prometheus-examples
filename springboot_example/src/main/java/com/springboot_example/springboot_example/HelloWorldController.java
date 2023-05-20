daeb1fedf373da5babc6fb103c037e3123501c0a
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2023-05-21 00:15:27 +0530

    added springboot custom metrics

--- springboot_example/src/main/java/com/springboot_example/springboot_example/HelloWorldController.java
+++ springboot_example/src/main/java/com/springboot_example/springboot_example/HelloWorldController.java
@@ -3,11 +3,23 @@ package com.springboot_example.springboot_example;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
 @RestController
 public class HelloWorldController {
-   
-    @GetMapping("/hello")
-    public String hello() {
-        return "World";
+  
+    Counter visitCounter;
+
+    public HelloWorldController(MeterRegistry registry) {
+        visitCounter = Counter.builder("visit_counter")
+            .description("Number of visits to the site")
+            .register(registry);
+    }
+            
+    @GetMapping("/")
+    public String index() {
+        visitCounter.increment();
+        return "Hello World!";
     }
 }
